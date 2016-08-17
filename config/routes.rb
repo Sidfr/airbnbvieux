8724c991a934b4f6ffe3a387ed0c34e3b1b71b07
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-17 16:09:38 +0200

    creation homepage with CSS and footer for all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :services, only: [:show, :index]
+
+  devise_for :users,
+     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root to: 'pages#home'
+
+resources :services, only: [:show, :index]
   namespace :account do
     resource :profile, controller: 'profile'
     resources :reviews, only: [:index, :show]
     resources :services
   end
-  devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
